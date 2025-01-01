4a476a60a7dd3c6372d9b4806be6ff9556324bfb
ayoub yousfi <ayoub-yousfi@live.Com>
2025-01-01 13:19:00 +0100

    try with different times

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from datetime import datetime, time as dt_time  # Import 'time' from datetime
 
 # Your Brevo API key
 BREVO_KEY = os.getenv("BREVO_KEY")
@@ -8,12 +9,22 @@ if not BREVO_KEY:
 # API endpoint for sending transactional emails
 url = "https://api.brevo.com/v3/smtp/email"
 
+current_time = datetime.now().time()  # Get current time
+morning_time = dt_time(7, 0)          # 7:00 AM
+midDay_time = dt_time(12, 0)          # 12:00 PM    
+if morning_time <= current_time < midDay_time:
+    dua = "اللّهُمَّ إِنِّي أَصْبَحْتُ أُشْهِدُكَ وَأُشْهِدُ حَمَلَةَ عَرْشِكَ وَجَمِيعَ خَلْقِكَ أَنَّكَ أَنتَ اللّهُ لَا إِلٰهَ إِلَّا أَنتَ وَحْدَكَ لَا شَرِيكَ لَكَ وَأَنَّ مُحَمَّدًا عَبْدُكَ وَرَسُولُكَ"
+elif midDay_time <= current_time < dt_time(23, 59, 59):  # Before midnight
+    dua = "اللهمَّ إنّي أمسيتُ أشهدُكَ وأشهِدُ حملة عرشِك وملائكتِك وجميع خلقك، أنّك أنتَ الله لا إلهَ إلا أنت، وحدك لا شريكَ لك، وأنّ محمداً عبدُك ورسولك"
+else:  # Between midnight and morning
+    dua = "لا إله إلا الله وحده لا شريك له، له الملك وله الحمد وهو على كل شيء قدير، سبحان الله، والحمد لله، ولا إله إلا الله، والله أكبر، ولا حول ولا قوة إلا بالله"
+
 # Payload with email details
 payload = {
     "sender": {"name": "Dua", "email": "ayoub.yousfi.enetcom@gmail.com"},
     "to": [{"email": "ayoub.yousfi.enetcom@gmail.com", "name": "Ayoub"}],
     "subject": "Morning Dua",
-    "htmlContent": "اللّهُمَّ إِنِّي أَصْبَحْتُ أُشْهِدُكَ وَأُشْهِدُ حَمَلَةَ عَرْشِكَ وَجَمِيعَ خَلْقِكَ أَنَّكَ أَنتَ اللّهُ لَا إِلٰهَ إِلَّا أَنتَ وَحْدَكَ لَا شَرِيكَ لَكَ وَأَنَّ مُحَمَّدًا عَبْدُكَ وَرَسُولُكَ"
+    "htmlContent": dua
 }
 
 # Headers for API request
