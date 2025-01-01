d30db3ed284a645cdb8742c8a79b138c4db32892
ayoub yousfi <ayoub-yousfi@live.Com>
2025-01-01 13:23:14 +0100

    adjsut the dua name

--- script.py
+++ script.py
@@ -14,16 +14,19 @@ morning_time = dt_time(7, 0)          # 7:00 AM
 midDay_time = dt_time(12, 0)          # 12:00 PM    
 if morning_time <= current_time < midDay_time:
     dua = "اللّهُمَّ إِنِّي أَصْبَحْتُ أُشْهِدُكَ وَأُشْهِدُ حَمَلَةَ عَرْشِكَ وَجَمِيعَ خَلْقِكَ أَنَّكَ أَنتَ اللّهُ لَا إِلٰهَ إِلَّا أَنتَ وَحْدَكَ لَا شَرِيكَ لَكَ وَأَنَّ مُحَمَّدًا عَبْدُكَ وَرَسُولُكَ"
+    duaTime = "Morning Dua"
 elif midDay_time <= current_time < dt_time(23, 59, 59):  # Before midnight
     dua = "اللهمَّ إنّي أمسيتُ أشهدُكَ وأشهِدُ حملة عرشِك وملائكتِك وجميع خلقك، أنّك أنتَ الله لا إلهَ إلا أنت، وحدك لا شريكَ لك، وأنّ محمداً عبدُك ورسولك"
+    duaTime = "Midday Dua"
 else:  # Between midnight and morning
     dua = "لا إله إلا الله وحده لا شريك له، له الملك وله الحمد وهو على كل شيء قدير، سبحان الله، والحمد لله، ولا إله إلا الله، والله أكبر، ولا حول ولا قوة إلا بالله"
+    duaTime = "Midnight Dua"
 
 # Payload with email details
 payload = {
     "sender": {"name": "Dua", "email": "ayoub.yousfi.enetcom@gmail.com"},
     "to": [{"email": "ayoub.yousfi.enetcom@gmail.com", "name": "Ayoub"}],
-    "subject": "Morning Dua",
+    "subject": duaTime,
     "htmlContent": dua
 }
 
